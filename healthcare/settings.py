674f503bf3ea97ebc1a0f2caf121bd23e719904b
DGclasher <73327721+DGclasher@users.noreply.github.com>
2024-02-27 10:31:55 +0530

    fix

--- healthcare/settings.py
+++ healthcare/settings.py
@@ -1,8 +1,7 @@
-import os
 
-from pathlib import Path
 import os
 import pymysql
+from pathlib import Path
 pymysql.install_as_MySQLdb()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -63,20 +62,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'healthcare.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'healthcare_db',
-#         'USER':'clasher',
-#         'PASSWORD':'M@tech@pp1234',
-#         'HOST':'127.0.0.1',
-#         'PORT':'3306'
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
